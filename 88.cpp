a469d4c998f885c43ef36c7c6f270557447e09d8
felipeschawsnekker <felipesilvamoura2024@gmail.com>
2025-04-20 15:22:32 -0300

    Create 88.cpp

---
+++ 88.cpp
@@ -0,0 +1,17 @@
+
+
+#include <iostream>
+using namespace std;
+
+int main (){
+    int cont=0;
+    for (int i=25; i<=200; i++){
+        if(i%2==0){
+        cont=cont+i;
+        }
+    
+}
+cout<<cont;
+
+return 0;
+}
