2d831da8d02cc810ad2976b81b49ce8e23e82058
felipeschawsnekker <felipesilvamoura2024@gmail.com>
2025-05-25 19:26:40 -0300

    Update 112.cpp

--- 112.cpp
+++ 112.cpp
@@ -2,37 +2,35 @@
 using namespace std;
 
 int main() {
-    const int tamanho = 25;
-    int A[tamanho], B[tamanho], V[tamanho * 2];
+    const int tamanho = 100;
+    int v[tamanho];
+    int iguais30 = 0;
+    int soma = 0;
+    double media;
+    int maioresMedia = 0;
+    int iguaisMedia = 0;
 
     for (int i = 0; i < tamanho; i++) {
-        cin >> A[i];
-    }
-
-    for (int i = 0; i < tamanho; i++) {
-        cin >> B[i];
-    }
-
-    for (int i = 0, j = 0; i < tamanho; i++) {
-        V[j++] = A[i];
-        V[j++] = B[i];
-    }
-
-    // Bubble sort em ordem decrescente
-    for (int i = 0; i < 49; i++) {
-        for (int j = 0; j < 49 - i; j++) {
-            if (V[j] < V[j + 1]) {
-                int temp = V[j];
-                V[j] = V[j + 1];
-                V[j + 1] = temp;
-            }
+        v[i] = i + 1;  
+        soma += v[i];
+        if (v[i] == 30) {
+            iguais30++;
         }
     }
 
-    for (int i = 0; i < 50; i++) {
-        cout << V[i] << " ";
+    media = soma / (double)tamanho;
+
+    for (int i = 0; i < tamanho; i++) {
+        if (v[i] > media) {
+            maioresMedia++;
+        } else if (v[i] == media) {
+            iguaisMedia++;
+        }
     }
 
-    cout << endl;
+    cout << "Numeros iguais a 30: " << iguais30 << endl;
+    cout << "Numeros maiores que a media: " << maioresMedia << endl;
+    cout << "Numeros iguais a media: " << iguaisMedia << endl;
+
     return 0;
 }
