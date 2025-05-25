140b29d11cb22aff07c18ebb1a21a1446d23d2e8
felipeschawsnekker <felipesilvamoura2024@gmail.com>
2025-05-25 19:23:57 -0300

    Update 111.cpp

--- 111.cpp
+++ 111.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main() {
     const int tamanho = 25;
-    int A[tamanho], B[tamanho], C[tamanho * 2];
+    int A[tamanho], B[tamanho], V[tamanho * 2];
 
     for (int i = 0; i < tamanho; i++) {
         cin >> A[i];
@@ -14,12 +14,23 @@ int main() {
     }
 
     for (int i = 0, j = 0; i < tamanho; i++) {
-        C[j++] = A[i];
-        C[j++] = B[i];
+        V[j++] = A[i];
+        V[j++] = B[i];
     }
 
-    for (int i = 0; i < tamanho * 2; i++) {
-        cout << C[i] << " ";
+    // Bubble sort em ordem decrescente
+    for (int i = 0; i < 49; i++) {
+        for (int j = 0; j < 49 - i; j++) {
+            if (V[j] < V[j + 1]) {
+                int temp = V[j];
+                V[j] = V[j + 1];
+                V[j + 1] = temp;
+            }
+        }
+    }
+
+    for (int i = 0; i < 50; i++) {
+        cout << V[i] << " ";
     }
 
     cout << endl;
